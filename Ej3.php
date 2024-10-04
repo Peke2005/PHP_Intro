3de074ac4fafa0b96058aba5a421fd28e7251657
Peke2005 <polcarbajalgarcia@gmail.com>
2024-10-04 18:17:29 +0200

    Ej 3 Modificado

--- Ej3.php
+++ Ej3.php
@@ -2,14 +2,12 @@
     $num1 = rand(11, 20);
     $num2 = rand(11, 20);
 
-    /* For del numero uno */
     echo "Ejercicio For <br>";
     echo "Numero 1 <br>";
     echo "El numero uno es: $num1 <br>";
     for($i = 0; $i <= $num1; $i++){
         echo "$i | <br>";
     }
-    /* For del numero dos */
     echo "----------------------------------------------------------------------------------------------------------------------------------------------------------------- <br>";
     echo "Ejercicio For <br>";
     echo "Numero 2 <br>";
@@ -22,7 +20,6 @@
     echo "Ejercicio While <br>";
     echo "Numero 1 <br>";
     echo "El numero uno es: $num1 <br>";
-    /* While del numero uno */
     $i = 0;
     while($i <= $num1){
         echo "$i | <br>";
@@ -33,7 +30,6 @@
     echo "Ejercicio While <br>";
     echo "Numero 2 <br>";
     echo "El numero dos es: $num2 <br>";
-    /* While del numero dos */
     $i2 = 0;
     while($i2 <= $num2){
         echo "$i2 | <br>";
@@ -44,7 +40,6 @@
     echo "Ejercicio Do While <br>";
     echo "Numero 1 <br>";
     echo "El numero uno es: $num1 <br>";
-    /* Do While del numero uno */
     $i3 = 0;
     do{
         echo "$i3 | <br>";
@@ -56,7 +51,6 @@
     echo "Ejercicio Do While <br>";
     echo "Numero 2 <br>";
     echo "El numero dos es: $num2 <br>";
-    /* Do While del numero uno */
     $i4 = 0;
     if($num1 > $num2){
         echo $num1;
